11a8166f96ad0d4b7ed5d5e7b4252877aece993e
Lita Beach <litabeach123@gmail.com>
2021-03-05 13:45:50 -0600

    viewEmployees appears as table, has partial info

--- employees.js
+++ employees.js
@@ -20,11 +20,16 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+
+// SELECT name, quote, network FROM tvcharacter INNER JOIN tvshow ON tvcharacter.showid = tvshow.showid
+
+// SELECT name, title from tvshow LEFT JOIN tvcharacter ON tvcharacter.showid = tvshow.showid WHERE genre = "Drama";
+
 function viewEmployees() {
-  let sql = "SELECT first_name, last_name FROM employees_db.employee";
+  let sql = "SELECT * FROM employees_db.employee LEFT JOIN role on role.id = employee.id ";
   connection.query(sql, function(err, res) {
     if (err) throw err;
-    console.log(res);
+    console.table(res);
   });
 }
 
@@ -32,7 +37,7 @@ function viewEmployeesDept() {
   let sql = "SELECT * FROM employees_db.employee";
   connection.query(sql, function(err, res) {
     if (err) throw err;
-    console.log(res);
+    console.table(res);
   });
 }
 
